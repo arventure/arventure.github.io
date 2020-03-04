b5360e36610c0e453656329ba95df542bed3364f
User <jmgarcia@arventure.com.ar>
2020-03-04 16:19:04 -0300

    Trello

--- Flutter/flutter_service_worker.js
+++ Flutter/flutter_service_worker.js
@@ -1,13 +1,13 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets\FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "4a4a958eb9b288ab897150b8d899a84f",
-"/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+  "/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"/assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/LICENSE": "77ed32b405ade84c62753e3f75453a99",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/index.html": "4676601253a50da6e25fe92145e522fa",
-"/main.dart.js": "39d15dea8f485a905d2d644540f41e89"
+"/main.dart.js": "6dc73954584f4d5dc7135a2e0cb30759"
 };
 
 self.addEventListener('activate', function (event) {
@@ -29,9 +29,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
